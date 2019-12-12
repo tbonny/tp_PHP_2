b5511375e5efe708086ee6ee784265bccb7bd710
tbonny <tbonny@la-providence.net>
2019-12-12 11:27:52 +0100

    fin exercice 4

--- exercice 4/index_game.php
+++ exercice 4/index_game.php
@@ -7,15 +7,22 @@ $scout = new personnage ();
 $heavy->setPseudo("heavy weapon guy ");
 $scout->setPseudo("scout ");
 
+
 $scout -> afficher();
 $heavy -> afficher();
 
 while ($heavy->estVivant() && $scout->estVivant())
-    if (rand(0,1))
 {
-    $heavy->attaquer($scout);
-} else {
-    $scout->attaquer($heavy);
+    $chance = rand(0,1);
+
+    if ($chance == 0)
+    {
+        $heavy->attaquer($scout);
+    } else
+        {
+            $scout->attaquer($heavy);
+        }
 }
+
 
 ?>
--- exercice 4/personnages.php
+++ exercice 4/personnages.php
@@ -4,12 +4,12 @@
 
         private $_pseudo;
         private $_vie;
-        private $_estVivant;
+        private $_estVivant = true;
 
         public function __construct()
         {
             $this->_vie = 100;
-            $this->_estVivant = true;
+            
         }
 
         public function setPseudo($nouveauPseudo)
@@ -23,24 +23,58 @@
            return  $this->_pseudo ;
         }
 
+        public function getVie()
+        {
+           return  $this->_vie ;
+        }
+
 
         public function afficher()
         {
             echo "<p>je suis ".$this->_pseudo." et j'ai ". $this->_vie ." pv</p>";
         }
 
-        public function attaquer($personnage)
+        public function prendDesDegat($degat)
         {
- 
-            $this->_vie = $this->_vie-20;
 
-            if ($this->_vie<=0)
+            $this->_vie = $this->_vie - $degat;
+           
+        }
+        
+
+        public function attaquer($personnage)
+        {            
+                    $critchance = rand(0,1);
+
+                    if ($critchance == 0){
+                        $personnage->prendDesDegat(20);
+                        echo "<p>".$this->_pseudo." attaque ".$personnage->getPseudo()." il perd 20 pv, il lui reste ". $personnage->getVie() ." pv</p>";
+                    } else {
+                        $personnage->prendDesDegat(50);
+                        echo "<p>RANDOM CRIT : ".$this->_pseudo." attaque ".$personnage->getPseudo()." il perd 50 pv, il lui reste ".$personnage->getVie()  ." pv</p>";
+                           }
+                   
+             if ($this->_vie <= 0)
+             {
+                $this->_estVivant = false;
+                echo "<p>".$this->_pseudo." attaque ".$personnage->getPseudo()." ,il lui reste 0 pv</p>";;
+                
+                $chance = rand(0,3);
+
+                if ($chance == 0)
                 {
-                    $this->_estVivant=false;
-                }
-            
-
-            echo "<p>".$this->_pseudo."est attaqué par ".$personnage->getPseudo()." et perd 20 pv, il lui reste ". $this->_vie ." pv</p>";
+                    echo "<p>".$this->_pseudo." a achevé ".$personnage->getPseudo()."</p>";
+                } elseif ($chance == 1)
+                    {
+                        echo "<p>".$this->_pseudo." a pulvérisé ".$personnage->getPseudo()."</p>";
+                    } elseif ($chance == 2)
+                        {
+                            echo "<p>".$this->_pseudo." a anihilé ".$personnage->getPseudo()."</p>";
+                        } elseif ($chance == 3)
+                            {
+                                echo "<p>".$this->_pseudo." a démembré ".$personnage->getPseudo()."</p>";
+                            }
+             }      
         }
 
         public function estVivant()
